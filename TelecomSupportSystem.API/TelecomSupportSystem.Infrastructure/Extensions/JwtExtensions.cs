1a5cbfbc54e7f5219c3d653638e6fca73bf7e9f5
Ihor Benderuk <igor19benderuk@gmail.com>
2026-04-07 21:42:16 +0200

    Separated by scopes, implemented Jwt issuing and basic checking

---
+++ TelecomSupportSystem.API/TelecomSupportSystem.Infrastructure/Extensions/JwtExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using TelecomSupportSystem.Infrastructure.Options;
+
+namespace TelecomSupportSystem.Infrastructure.Extensions
+{
+    public static class JwtExtensions
+    {
+        public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(opt =>
+                {
+                    var options = configuration.GetSection("Jwt").Get<JwtOptions>()
+                        ?? throw new InvalidOperationException("Jwt configuration section is missing");
+
+                    opt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+
+                        ValidIssuer = options.Issuer,
+                        ValidAudience = options.Audience,
+
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(options.Key))
+                    };
+                });
+
+            return services;
+        }
+    }
+}
