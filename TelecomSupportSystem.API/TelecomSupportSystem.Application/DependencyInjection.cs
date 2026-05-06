b317be3ee0c364e273254625fad9e6acc4bcc210
Ihor Benderuk <igor19benderuk@gmail.com>
2026-05-06 23:47:16 +0200

    Implemented registration functionality

---
+++ TelecomSupportSystem.API/TelecomSupportSystem.Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TelecomSupportSystem.Application.Interfaces;
+using TelecomSupportSystem.Application.Services;
+
+namespace TelecomSupportSystem.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthService, AuthService>();
+            return services;
+        }
+    }
+}
---
+++ TelecomSupportSystem.API/TelecomSupportSystem.Application/Interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using TelecomSupportSystem.Application.DTOs;
+using TelecomSupportSystem.Domain.Common;
+
+namespace TelecomSupportSystem.Application.Interfaces
+{
+    public interface IAuthService
+    {
+        public Task<Result> RegisterCustomerUser(RegisterRequest registerUserRequest);
+    }
+}
