1562fb46ba4dfb0c606966f01c91a01b4ce645aa
Ihor Benderuk <igor19benderuk@gmail.com>
2026-05-06 23:45:51 +0200

    Implemented RegisterRequest to AppUser map

---
+++ TelecomSupportSystem.API/TelecomSupportSystem.Application/Mappings/AppUserMapping.cs
@@ -0,0 +1,20 @@
+﻿using TelecomSupportSystem.Application.DTOs;
+using TelecomSupportSystem.Domain.Entities;
+
+namespace TelecomSupportSystem.Application.Mappings
+{
+    public static class AppUserMapping
+    {
+        public static AppUser ToAppUser(this RegisterRequest registerRequest)
+        {
+            return new AppUser()
+            {
+                Email = registerRequest.Email,
+                FirstName = registerRequest.FirstName,
+                LastName = registerRequest.LastName,
+                UserName = registerRequest.Email
+            };
+        }
+    }
+
+}
