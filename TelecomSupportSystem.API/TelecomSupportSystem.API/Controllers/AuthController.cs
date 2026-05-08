30ede95731a811dc7721eb630ea99ee26c16841e
Ihor Benderuk <igor19benderuk@gmail.com>
2026-05-08 17:21:19 +0200

    Implemented Customer Registration Endpoint

---
+++ TelecomSupportSystem.API/TelecomSupportSystem.API/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using TelecomSupportSystem.Application.DTOs;
+using TelecomSupportSystem.Application.Interfaces;
+
+namespace TelecomSupportSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterCustomer(RegisterRequest registerRequest)
+        {
+            var result = await _authService.RegisterCustomerUser(registerRequest);
+
+            if ( !result.IsSuccess )
+            {
+                return BadRequest(result.Error);
+            }
+            return Ok(result.IsSuccess);
+        }
+    }
+}
--- TelecomSupportSystem.API/TelecomSupportSystem.API/Program.cs
+++ TelecomSupportSystem.API/TelecomSupportSystem.API/Program.cs
@@ -1,4 +1,8 @@
-﻿using TelecomSupportSystem.Infrastructure;
+﻿using Scalar.AspNetCore;
+using TelecomSupportSystem.API.Middleware;
+using TelecomSupportSystem.Application;
+using TelecomSupportSystem.Infrastructure;
+using TelecomSupportSystem.Infrastructure.Persistence.Seeders;
 
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
@@ -7,15 +11,29 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 builder.Services.AddInfrastructure(config);
+builder.Services.AddApplication();
+
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+
+builder.Services.AddHostedService<DatabaseInitializer>();
+
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
+
 
 if ( app.Environment.IsDevelopment() )
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
+app.UseExceptionHandler();
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
