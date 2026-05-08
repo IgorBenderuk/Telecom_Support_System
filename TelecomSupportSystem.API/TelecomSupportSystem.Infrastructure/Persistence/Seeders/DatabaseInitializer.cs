b0306e4ecce2b16cf7caeab090581229f92801a1
Ihor Benderuk <igor19benderuk@gmail.com>
2026-05-08 17:19:39 +0200

    Added Db initializer for seeding the roles

---
+++ TelecomSupportSystem.API/TelecomSupportSystem.Infrastructure/Persistence/Seeders/DatabaseInitializer.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TelecomSupportSystem.Domain.Entities;
+
+namespace TelecomSupportSystem.Infrastructure.Persistence.Seeders
+{
+    public class DatabaseInitializer : IHostedService
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public DatabaseInitializer(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            using var scope = _serviceProvider.CreateScope();
+
+            var roleManager = scope.ServiceProvider
+                .GetRequiredService<RoleManager<IdentityRole>>();
+
+            foreach ( var role in new[] { Roles.Agent, Roles.Customer } )
+            {
+                if ( !await roleManager.RoleExistsAsync(role) )
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+            => Task.CompletedTask;
+    }
+}
