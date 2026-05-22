0f93fe840ef1d6b6a1f683fa30e9f02e475b205b
Ihor Benderuk <igor19benderuk@gmail.com>
2026-05-22 19:02:06 +0200

    Updated App User Configuration, and migrate db to ensure email uniqueness

---
+++ TelecomSupportSystem.API/TelecomSupportSystem.Infrastructure/Persistence/Migrations/20260522135727_NormalizedEmailUniqueness.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TelecomSupportSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class NormalizedEmailUniqueness : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "EmailIndex",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastChatActivity",
+                table: "Tickets",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateIndex(
+                name: "EmailIndex",
+                table: "AspNetUsers",
+                column: "NormalizedEmail",
+                unique: true,
+                filter: "[NormalizedEmail] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "EmailIndex",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastChatActivity",
+                table: "Tickets");
+
+            migrationBuilder.CreateIndex(
+                name: "EmailIndex",
+                table: "AspNetUsers",
+                column: "NormalizedEmail");
+        }
+    }
+}
