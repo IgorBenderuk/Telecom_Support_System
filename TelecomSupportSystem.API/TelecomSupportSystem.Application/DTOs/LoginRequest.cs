852af0cd849afc4b0c6fdf1a9484c4d643ad04d0
Ihor Benderuk <igor19benderuk@gmail.com>
2026-05-22 15:06:28 +0200

    Implemented logIn request & response DTOS

---
+++ TelecomSupportSystem.API/TelecomSupportSystem.Application/DTOs/LoginRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TelecomSupportSystem.Application.DTOs
+{
+    public class LoginRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(8)]
+        public string Password { get; set; }
+    }
+}
---
+++ TelecomSupportSystem.API/TelecomSupportSystem.Application/DTOs/LoginResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace TelecomSupportSystem.Application.DTOs
+{
+    public class LoginResponse
+    {
+        public string AccessToken { get; set; }
+    }
+}
