43d0d2f85108302d7b4260c5617ba35b6e8fe122
Ihor Benderuk <igor19benderuk@gmail.com>
2026-07-06 15:05:51 +0200

    Created MessageDto mapping extension, udpated naming for AppUserMapping

---
+++ TelecomSupportSystem.API/TelecomSupportSystem.Application/DTOs/Ticket/MessageDto.cs
@@ -0,0 +1,6 @@
+﻿using TelecomSupportSystem.Domain.Entities.TiketAgregate.Enums;
+
+namespace TelecomSupportSystem.Application.DTOs.Ticket
+{
+    public record MessageDto(int Id, string Content, MessageSenderType SenderType, DateTime SentAt);
+}
