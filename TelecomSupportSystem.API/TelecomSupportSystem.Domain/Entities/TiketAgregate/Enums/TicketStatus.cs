7ef157e730f3ed60500dfe10bc2c725a79dce9d5
Ihor Benderuk <igor19benderuk@gmail.com>
2026-07-17 20:02:34 +0200

    Serialize TicketStatus and MessageSenderType as string instead of int for a more stable API contract

--- TelecomSupportSystem.API/TelecomSupportSystem.Domain/Entities/TiketAgregate/Enums/TicketStatus.cs
+++ TelecomSupportSystem.API/TelecomSupportSystem.Domain/Entities/TiketAgregate/Enums/TicketStatus.cs
@@ -1,5 +1,8 @@
-﻿namespace TelecomSupportSystem.Domain.Entities.TiketAgregate.Enums
+﻿using System.Text.Json.Serialization;
+
+namespace TelecomSupportSystem.Domain.Entities.TiketAgregate.Enums
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum TicketStatus
     {
         Open,
